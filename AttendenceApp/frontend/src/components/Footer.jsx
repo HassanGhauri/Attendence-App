e1d395eaa29556c5d04da5597135ddf0aa2d62b6
Muhammad Hassan Ghauri <94550154+HassanGhauri@users.noreply.github.com>
2024-08-23 18:02:27 +0500

    Add files via upload

---
+++ AttendenceApp/frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { PiStudentFill } from "react-icons/pi";
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p><PiStudentFill /> Attend Inn @2024.</p>
+    </div>
+  )
+}
+
+export default Footer
